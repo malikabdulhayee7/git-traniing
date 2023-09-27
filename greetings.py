9fbcbed5c93fccede713cb294368228bb1370b06
abdul hayee <bc200403394@vu.edu.pk>
2023-09-28 00:14:19 +0500

    it is ready to be sumbited now

--- greetings.py
+++ greetings.py
@@ -6,6 +6,12 @@ for i in range(10):
 # write hello world 10 times with a for loop
 for i in range(10):
     print("Hello World")
+    # show some cool greetings images
+    # show some cool greetings images
+    # show some cool greetings images
+ 
 
     print("abdulhayee")
 # write hello world 10 times with a for loop
+
+# ask the user what he/she wants to drink 
