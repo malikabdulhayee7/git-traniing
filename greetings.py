458329e543d95d01016482a6ec3af8bab4dc63fb
abdul hayee <bc200403394@vu.edu.pk>
2023-09-27 18:09:29 +0500

    added greetings.py

---
+++ greetings.py
@@ -0,0 +1,9 @@
+# write hello world
+print("Hello World")
+# write hello world 10 times
+for i in range(10):
+    print("Hello World")
+# write hello world 10 times with a for loop
+for i in range(10):
+    print("Hello World")
+# write hello world 10 times with a for loop
